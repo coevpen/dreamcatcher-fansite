a5e3f2fe270346df2c937ceeb9a4615a5c8f3410
coevpen <koinuevins@gmail.com>
2022-05-02 22:37:16 -0500

    modal added, working on bios for all members

--- src/components/MemberModal.js
+++ src/components/MemberModal.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {MdClose} from 'react-icons/md';
 
-function MemberModal(){
+function MemberModal({showModal, setShowModal}){
 
     return (
-        <div>
-            
-        </div>
+        <>
+        {showModal ? (
+            <div className="modal-wrapper" showModal={showModal}>
+                <img src={require(`../assets/dami.jpg`)} className="modal-img" alt="Dami"/>
+                <div className="modal">
+                    <h1>Dami</h1>
+                    <p>This is Dami's bio.</p>
+                </div>
+                <MdClose className="modal-close-btn" aria-label="Close modal" onClick={() => setShowModal(prev => !prev)}/>
+            </div>
+        ):null}
+        </>
     );
 }
 
--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../MemberModal';
+
+// member pictures
 import Dami from '../../assets/damiShot.jpg';
 import Gahyeon from '../../assets/gahyeonShot.jpg';
 import Jiu from '../../assets/jiuShot.jpg';
@@ -9,35 +12,90 @@ import Yoohyeon from '../../assets/yoohyeonShot.jpg';
 
 const Members = () => {
 
+    const [showModal, setShowModal] = useState(false);
+    const openModal = () =>{
+        setShowModal(prev => !prev)
+    }
+
+    const [currentBio, setCurrentBio] = useState();
+
+    const bios = [
+
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        },
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        },
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        },
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        },
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        },
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        },
+        {
+            Name: "",
+            image: "",
+            Birthdate: "",
+            Position: ""
+        }
+    ];
+
     return (
         <div className="headShot-rows">
             <div className="picContainer picOdd">
-                <img className="headShot" src={Dami} alt="Dami"/>
+                <img className="headShot" src={Dami} alt="Dami" onClick={openModal}/>
             </div>
 
             <div className="picContainer picEven">
-                <img className="headShot" src={Gahyeon} alt="Gahyeon"/>
+                <img className="headShot" src={Gahyeon} alt="Gahyeon" onClick={openModal}/>
             </div>
 
             <div className="picContainer picOdd">
-                <img className="headShot" src={Jiu} alt="Jiu"/>
+                <img className="headShot" src={Jiu} alt="Jiu" onClick={openModal}/>
             </div>
 
             <div className="picContainer picEven">
-                <img className="headShot" src={Sua} alt="Sua"/>
+                <img className="headShot" src={Sua} alt="Sua" onClick={openModal}/>
             </div>
 
             <div className="picContainer picOdd">
-                <img className="headShot" src={Yoohyeon} alt="Yoohyeon"/>
+                <img className="headShot" src={Yoohyeon} alt="Yoohyeon" onClick={openModal}/>
             </div>
 
             <div className="picContainer picEven">
-                <img className="headShot" src={Handong} alt="Handong"/>
+                <img className="headShot" src={Handong} alt="Handong" onClick={openModal}/>
             </div>
 
             <div className="picContainer picOdd">
-                <img className="headShot" src={Siyeon} alt="Siyeon"/>
+                <img className="headShot" src={Siyeon} alt="Siyeon" onClick={openModal}/>
             </div>
+
+            <Modal showModal={showModal} setShowModal={setShowModal} />
         </div>
     );
 };
