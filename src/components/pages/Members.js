cc000aa738d18fd95d27f65d3e4835ee02e91def
coevpen <koinuevins@gmail.com>
2022-05-02 20:15:18 -0500

    member photos added plus CSS

--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import Dami from '../../assets/damiShot.jpg';
+import Gahyeon from '../../assets/gahyeonShot.jpg';
+import Jiu from '../../assets/jiuShot.jpg';
+import Sua from '../../assets/suaShot.jpg';
+import Handong from '../../assets/handongShot.jpg';
+import Siyeon from '../../assets/siyeonShot.jpg';
+import Yoohyeon from '../../assets/yoohyeonShot.jpg';
 
 const Members = () => {
 
     return (
-        <div>
-            <p className="text-white">The members page is under construction</p>
+        <div className="headShot-rows">
+            <div className="picContainer picOdd">
+                <img className="headShot" src={Dami} alt="Dami"/>
+            </div>
+
+            <div className="picContainer picEven">
+                <img className="headShot" src={Gahyeon} alt="Gahyeon"/>
+            </div>
+
+            <div className="picContainer picOdd">
+                <img className="headShot" src={Jiu} alt="Jiu"/>
+            </div>
+
+            <div className="picContainer picEven">
+                <img className="headShot" src={Sua} alt="Sua"/>
+            </div>
+
+            <div className="picContainer picOdd">
+                <img className="headShot" src={Yoohyeon} alt="Yoohyeon"/>
+            </div>
+
+            <div className="picContainer picEven">
+                <img className="headShot" src={Handong} alt="Handong"/>
+            </div>
+
+            <div className="picContainer picOdd">
+                <img className="headShot" src={Siyeon} alt="Siyeon"/>
+            </div>
         </div>
     );
 };
