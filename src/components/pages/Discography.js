dc6f10f05a1a64207d023b45f584d3326ab5b4db
coevpen <koinuevins@gmail.com>
2022-06-09 15:26:34 -0500

    adding data for discography

--- src/components/pages/Discography.js
+++ src/components/pages/Discography.js
@@ -1,26 +1,134 @@
 import React from 'react';
+import Disco from './disco';
+
+const discography = [
+    {
+        title: "Dystopia: The Tree of Language",
+        image: require(),
+        country: "Korea",
+        album_type: "Album",
+        tracklist: []
+    },
+    {
+        title: "Apocalypse: Save Us",
+        image: require(),
+        country: "Korea",
+        album_type: "Album",
+        tracklist: []
+    },
+    {
+        title: "The Beginning of the End",
+        image: require(),
+        country: "Japan",
+        album_type: "Album",
+        tracklist: []
+    },
+    {
+        title: "Nightmare",
+        image: require(),
+        country: "Korea",
+        album_type: "Single",
+        tracklist: []
+    },
+    {
+        title: "Fall Asleep in the Mirror",
+        image: require(),
+        country: "Korea",
+        album_type: "Single",
+        tracklist: ["My Toys", "Good Night", "Lullaby", "Good Night (Instrumental)"]
+    },
+    {
+        title: "Dystopia: Lose Myself",
+        image: require(),
+        country: "Korea",
+        album_type: "Mini Album",
+        tracklist: []
+    },
+    {
+        title: "Dystopia: Road to Utopia",
+        image: require(),
+        country: "Korea",
+        album_type: "Mini Album",
+        tracklist: []
+    },
+    {
+        title: "Prequel",
+        image: require(),
+        country: "Korea",
+        album_type: "Mini Album",
+        tracklist: []
+    },
+    {
+        title: "Alone in the City",
+        image: require(),
+        country: "Korea",
+        album_type: "Mini Album",
+        tracklist: []
+    },
+    {
+        title: "The End of Nightmare",
+        image: require(),
+        country: "Korea",
+        album_type: "Mini Album",
+        tracklist: []
+    },
+    {
+        title: "Nightmare - Escape the ERA",
+        image: require(),
+        country: "Korea",
+        album_type: "Mini Album",
+        tracklist: []
+    },
+    {
+        title: "No More",
+        image: require(),
+        country: "Japan",
+        album_type: "Single",
+        tracklist: []
+    },
+    {
+        title: "Eclipse",
+        image: require(),
+        country: "Japan",
+        album_type: "Single",
+        tracklist: []
+    },
+    {
+        title: "Endless Night",
+        image: require(),
+        country: "Japan",
+        album_type: "Single",
+        tracklist: []
+    },
+    {
+        title: "What",
+        image: require(),
+        country: "Japan",
+        album_type: "Single",
+        tracklist: []
+    },
+    {
+        title: "You and I",
+        image: require(),
+        country: "Japan",
+        album_type: "Single",
+        tracklist: []
+    },
+    {
+        title: "Piri ~Fue wo Fuke~",
+        image: require(),
+        country: "Japan",
+        album_type: "Single",
+        tracklist: []
+    },
+
+]
 
 const Discography = () => {
 
     return (
         <div>
-            <p>The discography page is under construction</p>
-
-            <div className="disco-section">
-                <h2 className="disco-title underline">Korean Albums</h2>
-            </div>
-            
-            <div className="disco-section">
-                <h2 className="disco-title underline">Korean Mini Albums</h2>
-            </div>
-
-            <div className="disco-section"> 
-                <h2 className="disco-title underline">Japanese Albums</h2>
-            </div>
-
-            <div className="disco-section">
-                <h2 className="disco-title underline">Japanese Singles</h2>
-            </div>
+            <Disco discography={discography}/>
         </div>
     );
 };
